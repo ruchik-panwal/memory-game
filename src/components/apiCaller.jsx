15ca4cdd7c006b5b0482bf41c349d5dfc26cebf6
ruchik-panwal <ruchik.panwal@gmail.com>
2025-07-09 19:33:54 +0530

    commenting

--- src/components/apiCaller.jsx
+++ src/components/apiCaller.jsx
@@ -1,3 +1,31 @@
-export async function apiCaller(){
-    
+// This Calls the API n-times for getting pokemon
+
+export async function apiCaller() {
+  const ranArr = randomNumberArr(1, 500); //Array of 10 random numbers
+
+  // Getting an array of objects containing name and image of the pokemon
+  const results = await Promise.all( 
+    ranArr.map(async (num) => {
+      const pokeApi = await fetch("https://pokeapi.co/api/v2/pokemon/" + num);
+      const pokeApiJson = await pokeApi.json();
+      return {
+        name: pokeApiJson.name,
+        image: pokeApiJson.sprites.front_default,
+      };
+    })
+  );
+
+  return results;
+}
+
+// Generates an array of size 10 with random numbers for cards
+function randomNumberArr(min, max) {
+  const ranArr = new Set();
+
+  while (ranArr.size < 10) {
+    const num = Math.floor(Math.random() * (max - min + 1)) + min;
+    ranArr.add(num);
+  }
+
+  return Array.from(ranArr);
 }

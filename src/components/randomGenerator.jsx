730c04417e7425e10d68e935a074bacf62d7aa35
ruchik-panwal <ruchik.panwal@gmail.com>
2025-07-10 22:42:14 +0530

    k

---
+++ src/components/randomGenerator.jsx
@@ -0,0 +1,11 @@
+// Generates an array of size 10 with random numbers for cards
+export default function randomNumberArr(min, max) {
+  const ranArr = new Set();
+
+  while (ranArr.size < 10) {
+    const num = Math.floor(Math.random() * (max - min + 1)) + min;
+    ranArr.add(num);
+  }
+
+  return Array.from(ranArr);
+}
---
+++ src/storage.jsx
@@ -0,0 +1,52 @@
+export const pokeDefault = [
+  {
+    name: "Pokemon",
+    image:
+      "https://i.pinimg.com/736x/21/46/46/2146460e2bed8258ed63e4e43d91b566.jpg",
+  },
+  {
+    name: "Pokemon",
+    image:
+      "https://i.pinimg.com/736x/21/46/46/2146460e2bed8258ed63e4e43d91b566.jpg",
+  },
+  {
+    name: "Pokemon",
+    image:
+      "https://i.pinimg.com/736x/21/46/46/2146460e2bed8258ed63e4e43d91b566.jpg",
+  },
+  {
+    name: "Pokemon",
+    image:
+      "https://i.pinimg.com/736x/21/46/46/2146460e2bed8258ed63e4e43d91b566.jpg",
+  },
+  {
+    name: "Pokemon",
+    image:
+      "https://i.pinimg.com/736x/21/46/46/2146460e2bed8258ed63e4e43d91b566.jpg",
+  },
+  {
+    name: "Pokemon",
+    image:
+      "https://i.pinimg.com/736x/21/46/46/2146460e2bed8258ed63e4e43d91b566.jpg",
+  },
+  {
+    name: "Pokemon",
+    image:
+      "https://i.pinimg.com/736x/21/46/46/2146460e2bed8258ed63e4e43d91b566.jpg",
+  },
+  {
+    name: "Pokemon",
+    image:
+      "https://i.pinimg.com/736x/21/46/46/2146460e2bed8258ed63e4e43d91b566.jpg",
+  },
+  {
+    name: "Pokemon",
+    image:
+      "https://i.pinimg.com/736x/21/46/46/2146460e2bed8258ed63e4e43d91b566.jpg",
+  },
+  {
+    name: "Pokemon",
+    image:
+      "https://i.pinimg.com/736x/21/46/46/2146460e2bed8258ed63e4e43d91b566.jpg",
+  },
+];

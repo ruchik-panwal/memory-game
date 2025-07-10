5386870f7ee7f3e9993ca5bee904d51eed283fa5
ruchik-panwal <ruchik.panwal@gmail.com>
2025-07-10 23:39:46 +0530

    added 2 new boxes

--- src/storage.jsx
+++ src/storage.jsx
@@ -49,4 +49,14 @@ export const pokeDefault = [
     image:
       "https://i.pinimg.com/736x/21/46/46/2146460e2bed8258ed63e4e43d91b566.jpg",
   },
+  {
+    name: "Pokemon",
+    image:
+      "https://i.pinimg.com/736x/21/46/46/2146460e2bed8258ed63e4e43d91b566.jpg",
+  },
+  {
+    name: "Pokemon",
+    image:
+      "https://i.pinimg.com/736x/21/46/46/2146460e2bed8258ed63e4e43d91b566.jpg",
+  },
 ];

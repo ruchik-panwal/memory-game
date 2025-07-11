a52023a5ccd404c352ac308fed0ea28757b8713b
ruchik-panwal <ruchik.panwal@gmail.com>
2025-07-12 03:54:10 +0530

    added animation to cards using gsap

--- src/components/gameCard.jsx
+++ src/components/gameCard.jsx
@@ -1,17 +1,88 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { useRef } from "react";
+
 // Card display of pokemon
 export default function Card({ pokemon, nxtLvl }) {
+  const textRef = useRef();
+  const imgRef = useRef();
+  const { contextSafe } = useGSAP();
+
+  const mouseEnter = contextSafe(() => {
+    gsap.to(textRef.current, {
+      duration: 0.5,
+      fontSize: "2.7rem",
+      color: "black",
+    });
+
+    gsap.to(imgRef.current, {
+      duration: 0.5,
+      height: "170px",
+      y: 10,
+    });
+  });
+
+  const mouseLeave = contextSafe(() => {
+    gsap.to(textRef.current, {
+      duration: 0.5,
+      fontSize: "2.5rem",
+      color: "#AFAFAF",
+    });
+
+    gsap.to(imgRef.current, {
+      duration: 0.5,
+      height: "200px",
+      y: 0,
+    });
+  });
+
+  const clickChange = contextSafe(() => {
+    const tl = gsap.timeline();
+
+    tl.to("#loaders", {
+      display: "flex",
+      duration: 0.2,
+      height: "100%",
+      ease: "power1.out",
+      onComplete: () => {
+        nxtLvl();
+      },
+    }).to(
+      "#loaders",
+      {
+        duration: 0.2,
+        height: "0%",
+        display: "none",
+        ease: "power1.in",
+      },
+      "+=0.2"
+    );
+
+    return tl;
+  });
+
   return (
     <button
-      onClick={nxtLvl} //Checks for clickstatus
-      className="bg-white rounded-[10%] sm:h-[200px] h-[120px] cursor-pointer relative hover:shadow-[0_0_200px_10px_#ffffff6c]"
+      onClick={clickChange}
+      onMouseEnter={mouseEnter}
+      onMouseLeave={mouseLeave}
+      className="box-border bg-white rounded-[10%] sm:h-[200px] h-[120px] cursor-pointer relative"
     >
+      <div
+        id="loaders"
+        className="hidden bg-pokeYellow absolute z-11 rounded-[10%] w-[100%] inset-0"
+      ></div>
       <div className="flex justify-center items-center">
         <img
+          ref={imgRef}
           className="absolute z-10 sm:h-[200px] h-[115px] outline-white-solid"
           src={pokemon.image}
           alt={pokemon.name}
         />
-        <h1 className="hidden sm:block absolute z-0 inset-0 text-footerGray font-teko font-black text-[2.5rem]">
+        <h1
+          ref={textRef}
+          className="hidden sm:block absolute z-0 inset-0 text-footerGray font-teko font-black text-[2.5rem]"
+        >
           {pokemon.name}
         </h1>
       </div>
